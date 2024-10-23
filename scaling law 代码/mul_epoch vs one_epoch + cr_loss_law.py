0b0174a273c870c0cc05ac449ddb3ba09bc3f442
Tingkai Yan <160614961+thefatherofgithub@users.noreply.github.com>
2024-10-23 21:45:29 +0800

    Update mul_epoch vs one_epoch + cr_loss_law.py

--- scaling law 代码/mul_epoch vs one_epoch + cr_loss_law.py
+++ scaling law 代码/mul_epoch vs one_epoch + cr_loss_law.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import numpy as np
 import torch
-import torch.nn as 
+import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
